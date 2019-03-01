4fbd14011278fd2f32875dba0bafa24d04c80f06
Marcin Ratajczak <maratajczak@gmail.com>
2019-03-01 23:09:27 +0100

    Init project

---
+++ calculateDistancePoints.js
@@ -0,0 +1,22 @@
+const calculateDistancePoints = (distance, hillSize, kPoint) => {
+    let totalDistancePoints;
+    switch (true) {
+        case (hillSize == 'normal'):
+            totalDistancePoints = 60 + (2 * (distance - kPoint));
+            return totalDistancePoints;
+            break;
+        case (hillSize == 'big'):
+            totalDistancePoints = 60 + (1.8 * (distance - kPoint));
+            return totalDistancePoints;
+            break;
+        case (hillSize == 'mammoth'):
+            totalDistancePoints = 120 + (1.2 * (distance - kPoint));
+            return totalDistancePoints;
+            break;
+        default:
+            return 'We do not have any other type of hill';
+            break;
+    }
+};
+
+module.exports = calculateDistancePoints;
---
+++ calculateTotalPoints.js
@@ -0,0 +1,11 @@
+const calculateDistancePoints = require('./calculateDistancePoints');
+const calculateStylePoints = require('./calculateStylePoints');
+
+const calculateTotalPoints = (distance, hillSize, kPoint, styleNotes, gateFactor, windFactor) => {
+    const distancePoints = calculateDistancePoints(distance, hillSize, kPoint);
+    const stylePoints = calculateStylePoints(styleNotes);
+    totalPoints = distancePoints + stylePoints + gateFactor + windFactor;
+    return totalPoints.toFixed(1);
+}
+
+module.exports = calculateTotalPoints;
